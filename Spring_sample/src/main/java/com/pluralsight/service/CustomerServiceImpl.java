01d80665b8903f77ec2ed5caa08c79c6e176b2c9
John <johnconley507@hotmail.com>
2017-06-22 14:32:05 -0400

    adding spring dependencies to program

--- Spring_sample/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ Spring_sample/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -10,9 +10,7 @@ public class CustomerServiceImpl implements CustomerService {
 	
 	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
 	
-	/* (non-Javadoc)
-	 * @see com.pluralsight.service.CustomerService#findAll()
-	 */
+	
 	@Override
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
